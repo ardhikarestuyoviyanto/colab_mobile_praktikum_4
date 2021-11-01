4daf5faf260baebfbfcbeec7799078bef5fc3fd6
Ardhikarestu <ardhikayoviyanto@gmail.com>
2021-11-01 20:32:10 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <title>Halaman Templeate Sama Dengan Dosen</title>
+</head>
+<body>
+
+    <!-- Halaman Templeate Yang Sama Dengan Dosen -->
+    
+</body>
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+</html>
